bfd7be1a7b24d7a70ba84c311043f136b1e83ea6
Riccardo Ceccaroni <50733776+riccardoc95@users.noreply.github.com>
2024-02-02 15:39:15 +0100

    Update __init__.py

--- pixhomology/__init__.py
+++ pixhomology/__init__.py
@@ -1,1 +1,12 @@
+"""
+PixHomology.
+
+Calculation of Persistent Homology for Pixel Data.
+"""
+
 from .pixhom import calculatePH
+
+
+__version__ = "0.0.1"
+__author__ = 'Riccardo Ceccaroni'
+__credits__ = ''
