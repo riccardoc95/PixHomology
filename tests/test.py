30f06e2ec7a589d7e61c0ea150be1f6b3c093685
Riccardo Ceccaroni <50733776+riccardoc95@users.noreply.github.com>
2024-02-07 19:41:40 +0100

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,6 @@ import pixhomology as ph
 
 
 if __name__ == "__main__":
-    img = np.random.rand(100,100)
+    img = np.random.rand(100,100).astype(np.float32)
     dgm = ph.calculatePH(img)
     print(dgm)
