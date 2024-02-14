172ee73be38071a9f834eb9ecbab521343b9ee1a
Riccardo Ceccaroni <50733776+riccardoc95@users.noreply.github.com>
2024-02-14 08:50:16 +0100

    update test.py comparison

--- tests/ripser_comparison/test.py
+++ tests/ripser_comparison/test.py
@@ -42,37 +42,18 @@ def results_test(dataset, dataset_name='', idx=0):
         tracemalloc.stop()
         dgm_pixh = dgm_pixh.astype(np.float32)
 
-        dgm_rips = np.around(dgm_rips, 4)
-        dgm_pixh = np.around(dgm_pixh, 4)
-    
-        dgm_rips = sorted([tuple(dgm_rips[i]) for i in range(len(dgm_rips))])
-        dgm_pixh = sorted([tuple(dgm_pixh[i]) for i in range(len(dgm_pixh))])
-
-        # Experimental Comparison
-        # For large dgms the bottleneck distance may take a long time 
-        # or require a lot of memory. We propose this alternative comparison. 
-        # ATTENTION: the distance d could be incorrect due to approximations!
+        # Comparison
         if len(dgm_rips) == len(dgm_pixh):
-            for i in range(len(dgm_rips)):
-                p1 = dgm_rips[i]
-                p2 = dgm_pixh[i]
-    
-                d = np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
-                if d < 1e-04:
+            dgm_rips = dgm_rips[np.lexsort((dgm_rips[:,0],dgm_rips[:,1]))]
+            dgm_pixh = dgm_pixh[np.lexsort((dgm_pixh[:,0],dgm_pixh[:,1]))]
+
+            d = np.mean(np.sqrt((dgm_pixh[:,0] - dgm_rips[:,0]) ** 2 + (dgm_pixh[:,1] - dgm_rips[:,1]) ** 2))
+            if d < 1e-07:
                     continue
-                else:
-                    d_max = d
-                    for j in range(len(dgm_pixh)):
-                        p2 = dgm_pixh[j]
-                        d = np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
-                        if d < d_max:
-                            d_max = d
-                    if d_max < 1e-04:
-                        continue
-                    else:
-                        print(f"ERROR: the differences beetween 2 points in the dgm is {d}!")
-                        error = True
-                        #return False
+            else: 
+                print(f"ERROR: the differences beetween 2 points in the dgm is {d}!")
+                error = True
+                #return False
         else:
             print("ERROR: there are not the same number of points in the diagrams!")
             error = True
